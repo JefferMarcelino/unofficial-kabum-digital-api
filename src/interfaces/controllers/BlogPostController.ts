8ee0d33110cc4ed7732e2b0a112c8d1ffa0a4668
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-07-29 14:57:29 +0200

    feat: create controller for BlogPost

---
+++ src/interfaces/controllers/BlogPostController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { BlogPostRepository } from '../../domain/repositories/BlogPostRepository';
+
+export class BlogPostController {
+  constructor(private readonly blogPostRepository: BlogPostRepository) {};
+
+  async getPostDataById(req: Request, res: Response) {
+    const postId = req.params.id;
+
+    try {
+      const post = await this.blogPostRepository.getPostDataById(postId);
+
+      res.status(200).send({ "post": post }).end();
+    } catch (err) {
+      res.status(404).send({ "error": "Post not found" });
+    };
+  };
+
+  async getAllPostsFromPage(req: Request, res: Response) {
+    const page = Number(req.params.page);
+
+    if (Number.isNaN(page)) {
+      res.status(400).send({ "error": "page must be a number" });
+
+      return;
+    };
+
+    try {
+      const posts = await this.blogPostRepository.getPagePosts(page);
+
+      res.status(200).send({ "result": posts }).end();
+    } catch (error) {
+      res.status(500).send({ "error": "internal server error" })
+    };
+  };
+
+  async getAllPosts(req: Request, res: Response) {
+    try {
+      const posts = await this.blogPostRepository.getAllPosts();
+
+      res.status(200).send({ "result": posts }).end();
+    } catch (error) {
+      res.status(500).send({ "error": "internal server error" })
+    };
+  };
+
+  async getRandomPost(req: Request, res: Response) {
+    const randomPage = Math.floor(Math.random() * 30) + 1;
+    const posts = await this.blogPostRepository.getBlogPostsLinks("all", randomPage);
+
+    res.status(200).send({"posts": posts}).end()
+  };
+};
